4496e4d10a389bd9584aeada34f8665e4bc86d52
Nanzil <ahmnanzil33@gmail.com>
2023-03-05 13:03:31 +0600

    Changes theme and create login , signup page

--- CanteenManagementSystem/canteen/views.py
+++ CanteenManagementSystem/canteen/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+#it is login view
+def login(request):
+    return render(request, "login.html")
